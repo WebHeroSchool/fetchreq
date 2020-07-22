cfee1fca71e6fd2a37637695095a54e14eadc2c4
Valentina <61659523+deminavalentina22@users.noreply.github.com>
2020-07-22 08:10:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,47 @@
-
 const body = document.body;
 
 const urlParam = window.location.search.substring(1);
 const login = (urlParam.split(('='))[1]);
 
 let url = 'https://api.github.com/users/deminavalentina22';
-if(urlParam != '') {
-  url = `https://api.github.com/users/${login}`
-}
+  if(urlParam != '') {
+    url = `https://api.github.com/users/${login}`
+    }
 
 fetch(url)
-.then(response => {
-  if (response.status != 404) {
-    return response.json();
-  }
-  else {
-    let err = new Error(response.statusText + ' ' + response.status);
-    err.response = response;
-    throw err
-  }
-})
+  .then(response => {
+    if (response.status != 404) {
+      return response.json();
+    }
+    else {
+      let err = new Error(response.statusText + ' ' + response.status);
+      err.response = response;
+      throw err
+    }
+  })
 
-.then(json => {
- let ava = new Image();  
- ava.src = json.avatar_url;
- body.append(ava);
+  .then(json => {
+   let ava = new Image();  
+   ava.src = json.avatar_url;
+   body.append(ava);
  
- let name = document.createElement('p');
- name.classList.add('link');
- name.addEventListener("click", () => window.location = json.html_url);
- if (json.name != null) {
-  name.innerHTML = json.name;    
-} else {
-  name.innerHTML = 'Информация о пользователе недоступна';
-}
-body.append(name); 
+   let name = document.createElement('p');
+   name.classList.add('link');
+   name.addEventListener("click", () => window.location = json.html_url);
+    if (json.name != null) {
+      name.innerHTML = json.name;    
+    } else {
+      name.innerHTML = 'Информация о пользователе недоступна';
+    }
+    body.append(name); 
 
-let bio = document.createElement('p');
-if (json.bio != null) {
-  bio.innerHTML = json.bio;    
-} else {
- bio.innerHTML = 'Пользователь не заполнил данное поле';
-}
-body.append(bio);  
-})
+   let bio = document.createElement('p');
+    if (json.bio != null) {
+      bio.innerHTML = json.bio;    
+    } else {
+       bio.innerHTML = 'Пользователь не заполнил данное поле';
+    }
+    body.append(bio);  
+    })
 
-.catch(error => document.body.innerHTML = `Пользователь не найден.<br> ${error}`);
+    .catch(error => document.body.innerHTML = `Пользователь не найден.<br> ${error}`);
